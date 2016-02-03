68e458c09b171f27cdc76263e6797049196f1058
Steven McConomy <srmcconomy@gmail.com>
2016-02-03 01:01:14 -0500

    af

--- src/view/channels-pane.jsx
+++ src/view/channels-pane.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
 export default class ChannelsPane extends React.Component {
-  render() {
-    
+  render() {  
     return <div className="channels flex-vertical"></div>;
   }
 }
