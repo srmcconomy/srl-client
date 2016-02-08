2e7787caf0937d0d1c0243e7b2ee54c15520b783
Steven McConomy <srmcconomy@gmail.com>
2016-02-08 00:12:16 -0500

    asdf

--- src/view/channels-list-item.jsx
+++ src/view/channels-list-item.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import dateformat from '../dateformat'
 
 export default class ChannelsListItem extends React.Component {
   render() {
     let channel = this.props.channel;
     return (
       <div className="channels-list-item">
-        <div className="channel-name">{channel.name}</div>
-        <div className="channel-time">{channel.time}</div>
+        <div className="channel-name body2">{channel.name}</div>
+        <div className="channel-time caption">{(new Date(channel.time)).format('h:mm TT')}</div>
       </div>
     );
   }
